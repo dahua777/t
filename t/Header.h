34370c6fd40c437fa0fb6709198e8f6ddeea7b53
dahua777 <1220860737@qq.com>
2016-07-08 09:03:42 +0800

    通讯录
    链接数据库，制作添加按钮，绑定变量

---
+++ t/Header.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  t
+//
+//  Created by 陆天明 on 16/7/8.
+//  Copyright © 2016年 陆天明. All rights reserved.
+//
+#import "sqlite3.h"
+#import <time.h>
--- t/ViewController.swift
+++ t/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func tianjia(sender: AnyObject) {
+        let uname = self.name.text!
+        let mobile = self.num.text!
+        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        let result = db.execute(sql)
+        print(result)
+    }
+    @IBOutlet weak var num: UITextField!
+    @IBOutlet weak var name: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        db = SQLiteDB.sharedInstance()
+        
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        
     }
 
     override func didReceiveMemoryWarning() {
